73259f3a27b13bfdc5caae7fc6169100aa80fc00
FranciscoTorresC <juanfrancisco87@hotmail.com>
2024-05-06 23:11:23 -0500

    Undo: Testing super admin user in Users Table

--- components/adminPage.js
+++ components/adminPage.js
@@ -500,7 +500,6 @@ export async function pageAdmin(user) {
 
     const companyAdmin = adminInfo.company_admin;
     const basicAdmin = adminInfo.basic_admin;
-    const superAdmin = adminInfo.super_admin;
     const adminCompanyName = userInfo.user_company;
     console.log('admin:', adminCompanyName);
     let adminUsers = [];
@@ -588,7 +587,7 @@ export async function pageAdmin(user) {
       .then(data => {
         // Filter and set user data for the table based on admin privileges and company
         //console.log(data, 'admin', adminCompanyName);
-        table.setData((companyAdmin || basicAdmin || superAdmin) ? data.filter(user => user.companyID && user.companyID != ' ' && user.companyID != '' && user.companyID.some(x => adminCompanyName.includes(x))) : data);
+        table.setData((companyAdmin || basicAdmin) ? data.filter(user => user.companyID && user.companyID != ' ' && user.companyID != '' && user.companyID.some(x => adminCompanyName.includes(x))) : data);
       })
       .catch(err => {
         console.log('error fetching users', err);
